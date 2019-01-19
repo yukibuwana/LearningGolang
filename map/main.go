3576709cda00e830d088bb47fe9b0ef951dcde09
Yuki Buwana <yukibuwana@gmail.com>
2019-01-19 08:37:31 +0700

    Initial commit

--- map/main.go
+++ map/main.go
@@ -3,10 +3,22 @@ package main
 import "fmt"
 
 func main() {
+	// var colors map[string]string
+
+	// colors := make(map[string]string)
+	// colors["white"] = "ffffff"
+
 	colors := map[string]string{
 		"red":   "#ff0000",
-		"green": "00ff00",
+		"green": "#00ff00",
+		"white": "#ffffff",
 	}
 
-	fmt.Println(colors["red"])
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
